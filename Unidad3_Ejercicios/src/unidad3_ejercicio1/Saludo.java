42b399abe91a0a57a9fe96c1981c8fd2743176d3
nervi0s <nervi0s@protonmail.com>
2021-02-15 20:54:03 +0100

    Practices

---
+++ Unidad3_Ejercicios/src/unidad3_ejercicio1/Saludo.java
@@ -0,0 +1,50 @@
+package unidad3_ejercicio1;
+
+import java.io.Serializable;
+
+public class Saludo implements Serializable {
+
+    private String ciudad;
+    private String pais;
+    private String saludo;
+
+    public Saludo() {
+
+    }
+
+    public Saludo(String pais, String ciudad, String saludo) {
+        this.ciudad = ciudad;
+        this.pais = pais;
+        this.saludo = saludo;
+    }
+
+    public String getCiudad() {
+        return ciudad;
+    }
+
+    public void setCiudad(String ciudad) {
+        this.ciudad = ciudad;
+    }
+
+    public String getPais() {
+        return pais;
+    }
+
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+
+    public String getSaludo() {
+        return saludo;
+    }
+
+    public void setSaludo(String saludo) {
+        this.saludo = saludo;
+    }
+
+    @Override
+    public String toString() {
+        return "Pais=" + pais + ", Ciudad=" + ciudad + ", Saludo=" + saludo + ".";
+    }
+
+}

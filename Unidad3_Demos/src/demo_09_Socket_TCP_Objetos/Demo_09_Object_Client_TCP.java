a3ad48217f303e4c07d1cff92414588185ba1802
nervi0s <nervi0s@protonmail.com>
2021-02-15 19:29:17 +0100

    Import

--- Unidad3_Demos/src/demo_09_Socket_TCP_Objetos/Demo_09_Object_Client_TCP.java
+++ Unidad3_Demos/src/demo_09_Socket_TCP_Objetos/Demo_09_Object_Client_TCP.java
@@ -5,7 +5,6 @@ import java.io.InputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.OutputStream;
-import java.net.InetAddress;
 import java.net.Socket;
 import java.util.Scanner;
 
